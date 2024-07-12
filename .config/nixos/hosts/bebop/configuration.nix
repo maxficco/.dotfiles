38fa6b548753ebb491a1e2f8f559cd83694b3ed9
Max Ficco <maxwell.ficco@gmail.com>
2024-07-12 12:42:46 -0600

    vim

--- .config/nixos/hosts/bebop/configuration.nix
+++ .config/nixos/hosts/bebop/configuration.nix
@@ -42,6 +42,7 @@
         mpv
         syncthing
     ];
+    services.syncthing.enable = true;
 
     hardware.bluetooth.enable = true; # enables support for Bluetooth
     hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
