ee01f28d77263d09169ed0893e8801b5654fa588
Max Ficco <maxwell.ficco@gmail.com>
2025-01-23 18:35:57 -0700

    vim

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -4,9 +4,10 @@
 
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        ngrok.url = "github:ngrok/ngrok-nix";
     };
 
-    outputs = { self, nixpkgs, ... } @ inputs:
+    outputs = { self, nixpkgs, ngrok, ... } @ inputs:
         let
             lib = nixpkgs.lib;
         in {
@@ -21,6 +22,21 @@
                 system = "x86_64-linux";
                 modules = [ 
                     ./hosts/bebop/configuration.nix
+                    ngrok.nixosModules.ngrok
+                    ({ pkgs, ... }: {
+                      nixpkgs.config.allowUnfree = true;
+                      services.ngrok = {
+                        enable = true;
+                        extraConfig = { };
+                        extraConfigFiles = [
+                          # reference to files containing `authtoken` and `api_key` secrets
+                          # ngrok will merge these, together with `extraConfig`
+                        ];
+                        tunnels = {
+                          # ...
+                        };
+                      };
+                    })
                 ];
             };
         };
