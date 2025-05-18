a7e8a8bc602d1553d3f2989a6ca71aab41b4bc47
Max Ficco <maxwell.ficco@gmail.com>
2025-05-18 17:10:05 -0400

    vim

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -3,7 +3,7 @@
     description = "maxficco's flake";
 
     inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     };
 
     outputs = { self, nixpkgs, ... } @ inputs:
