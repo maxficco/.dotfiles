12341efdcf81e56126ceedea57f1db20a1f15b3c
Max Ficco <maxwell.ficco@gmail.com>
2025-07-03 15:45:29 -0400

    shhh

--- .config/nixos/hosts/bebop/secrets.nix
+++ .config/nixos/hosts/bebop/secrets.nix
@@ -1,4 +1,4 @@
 {
     # This isn't my actual token, using `git update-index --no-assume-unchanged secrets.nix`
-    frpToken = "40be6707bc6b7642b962d274fd44e8d89395d424391a64722b0509e7bdd94851";
+    frpToken = "password123";
 }
